165d928b5f797a94949c37388fee3485c2957af8
Josue Ruiz <josu24.42@gmail.com>
2024-06-24 10:29:56 -0600

    struggle

---
+++ Gracia Sublime es.pro
@@ -0,0 +1,23 @@
+{title: Gracia Sublime es}
+{artist: Open}
+[B]Quien rompe [Bsus-B]el poder     del pecado
+[E]su amor es[Esus-E]fuerte  y poderoso
+[G#m]El rey de gloria[F#] el rey de majesta[E]d
+
+[B] Conmueve al[Bsus-B]mundo con su estruendo
+[E] Y nos aso[Esus-E]mbra con maravillas
+[G#m]    El rey de gloria[F#] el rey de majesta[E]d
+[CORO]B                     E
+Gracia sublime es,   perfecto es tu amor
+Tomaste mi lugar[G#m]     cargaste tu mi c[F#]ruz
+Tu vida diste ah[B]í y ahora libre s[E]oy
+[G#m]Jesús te adoro [F#]    por lo que hiciste en m[E]í
+
+[B]Pusiste en o[Bsus-B]rden todo el caos
+[E]Nos adopt[Esus-E]aste  como tus hijos
+[G#m]   El rey de gloria[F#] el rey de majest[E]ad
+
+
+[B]El que gob[Bsus-B]ierna con su justicia
+[E] Y resplan[Esus-E]dece con su belleza
+[G#m]    El rey de gloria[F#] el rey de majest[E]ad
--- OnStageMain.py
+++ OnStageMain.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import font
+from tkinter import filedialog
+from pychordpro import chordpro
+import pychordpro
 
 class PrompterApp:
     def __init__(self, root):
@@ -28,17 +30,29 @@ class PrompterApp:
         self.tempo_entry = tk.Entry(root, textvariable=self.current_tempo)
         self.tempo_entry.pack()
         tk.Button(root, text="Display", command=self.display_content).pack()
+        tk.Button(root, text="Load ChordPro File", command=self.load_chordpro).pack()
         
         # Secondary display UI
-        self.lyrics_label = tk.Label(self.secondary, text="", font=("Helvetica", 32), fg="white", bg="black")
-        self.lyrics_label.pack(expand=True)
-        self.chords_label = tk.Label(self.secondary, text="", font=("Helvetica", 24), fg="yellow", bg="black")
-        self.chords_label.pack()
-        
         self.canvas = tk.Canvas(self.secondary, highlightthickness=0, bg='black')
         self.canvas.pack(fill=tk.BOTH, expand=True)
+        
+        self.lyrics_label = tk.Label(self.secondary, text="", font=("Helvetica", 32), fg="white", bg="black")
+        self.chords_label = tk.Label(self.secondary, text="", font=("Helvetica", 24), fg="yellow", bg="black")
+        
+        self.lyrics_window = self.canvas.create_window(0, 0, window=self.lyrics_label, anchor='center')
+        self.chords_window = self.canvas.create_window(0, 0, window=self.chords_label, anchor='center')
+
+        self.canvas.bind('<Configure>', self.resize_labels)
+        
         self.update_tempo()
         
+    def resize_labels(self, event):
+        width = event.width
+        height = event.height
+        
+        self.canvas.coords(self.lyrics_window, width / 2, height / 2 - 40)
+        self.canvas.coords(self.chords_window, width / 2, height / 2 + 40)
+    
     def display_content(self):
         lyrics = self.current_lyrics.get()
         chords = self.current_chords.get()
@@ -50,23 +64,50 @@ class PrompterApp:
         self.root.after(60000 // bpm, self.blink_tempo)
         
     def blink_tempo(self):
-        self.canvas.delete("all")
+        self.canvas.delete("border")
         width = self.secondary.winfo_width()
         height = self.secondary.winfo_height()
         border_thickness = 20
         
         current_color = self.canvas.cget("highlightbackground")
-        next_color = "yellow" if current_color == "black" else "black"
+        next_color = "red" if current_color == "black" else "black"
         
-        self.canvas.create_rectangle(0, 0, width, border_thickness, fill=next_color, outline="")
-        self.canvas.create_rectangle(0, height - border_thickness, width, height, fill=next_color, outline="")
-        self.canvas.create_rectangle(0, 0, border_thickness, height, fill=next_color, outline="")
-        self.canvas.create_rectangle(width - border_thickness, 0, width, height, fill=next_color, outline="")
+        # Top border
+        self.canvas.create_rectangle(0, 0, width, border_thickness, fill=next_color, outline="", tag="border")
+        # Bottom border
+        self.canvas.create_rectangle(0, height - border_thickness, width, height, fill=next_color, outline="", tag="border")
+        # Left border
+        self.canvas.create_rectangle(0, 0, border_thickness, height, fill=next_color, outline="", tag="border")
+        # Right border
+        self.canvas.create_rectangle(width - border_thickness, 0, width, height, fill=next_color, outline="", tag="border")
         
         self.canvas.config(highlightbackground=next_color)
         
         bpm = self.current_tempo.get()
         self.root.after(60000 // bpm, self.blink_tempo)
+
+    def load_chordpro(self):
+        file_path = filedialog.askopenfilename(filetypes=[("ChordPro files", "*.pro *.chopro")])
+        if not file_path:
+            return
+
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        parsed_song = content
+
+        lyrics = []
+        chords = []
+        for line in parsed_song:
+            for element in line:
+                if isinstance(element, pychordpro.Song.dump_chords):
+                    chords.append(f"[{element.name}]")
+                if isinstance(element, ):
+                    lyrics.append(element.text)
+        
+        self.current_lyrics.set(' '.join(lyrics))
+        self.current_chords.set(' '.join(chords))
+        self.display_content()
 
 if __name__ == "__main__":
     root = tk.Tk()
